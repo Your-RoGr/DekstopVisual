20952df979a7f91599aa67ead2244c4f0c1ba256
Extragenchik <kirillprorus@gmail.com>
2022-10-11 19:16:49 +0500

    V0.6

--- GetHorizontalPlot.py
+++ GetHorizontalPlot.py
@@ -1,17 +1,25 @@
+import os
+
 import pandas as pb
 import matplotlib.pyplot as plt
 import seaborn
 
-# from HandlerDataFrame_Class import HandlerDataFrame
+from HandlerDataFrame_Class import HandlerDataFrame
 
 def Main():
-    # объеденить данные, если одна скважика, или разбить на скважины, хз что в этих данных
-    carrotages = func1() # truple ([]) - все виды
+    for i in range(8):
+        las = HandlerDataFrame(f'{i+1}.las')
+        # объеденить данные, если одна скважика, или разбить на скважины, хз что в этих данных
+        carrotages = las.keys # truple ([]) - все виды
 
-    for carrotage in carrotages:
-        x, y = func2(carrotage)
 
-        plt.plot(x, y)
+        for carrotage in carrotages:
+            x, y = las.GetCarrotValue(carrotage)
+
+            fig, ax = plt.subplots(figsize=(4, 10))
+            plt.plot(x, y)
+            plt.savefig(f'{os.getcwd()}\\graphics\\{carrotage}-{i}.png')
+            plt.show()
 
 if __name__ == "__main__":
     Main()
--- HandlerDataFrame_Class.py
+++ HandlerDataFrame_Class.py
@@ -8,32 +8,16 @@ class HandlerDataFrame():
         super(HandlerDataFrame, self).__init__()
         self.name = name
 
-        self.las = lasio.read(rf'C:\\Users\\kiril\\OneDrive\\Рабочий стол\\For programist\\Programs\\DekstopVisual\\data\\GIS_md\\{name}')
+        self.dictionary = lasio.read(rf'C:\\Users\\kiril\\OneDrive\\Рабочий стол\\For programist\\Programs\\DekstopVisual\\data\\GIS_md\\{name}')
 
-        self.CarrotsName = self.las.keys()
+        self.keys = self.dictionary.keys()
 
-        self.DEPT = self.las['DEPT']
-
-        self.PZ_T = self.las['PZ_T']
-        self.GK_T = self.las['GK_T']
-        self.MPZ_T = self.las['MPZ_T']
-        self.MGZ_T = self.las['MGZ_T']
-        self.MBK_T = self.las['MBK_T']
-        self.BK_T = self.las['BK_T']
-        self.PS_T = self.las['PS_T']
-        self.IK_T = self.las['IK_T']
-        self.GZ1_T = self.las['GZ1_T']
-        self.GZ2_T = self.las['GZ2_T']
-        self.GZ3_T = self.las['GZ3_T']
-        self.GZ4_T = self.las['GZ4_T']
-        self.GZ5_T = self.las['GZ5_T']
 
     def GetCarrotValue(self, CarrotName):
-        y = self.las['DEPT']
-
-        self.CarrotsName = self.las[CarrotName]
-        x = self.CarrotsName
-        return x
+        self.CarrotName = self.dictionary[CarrotName]
+        x = self.CarrotName
+        y = self.dictionary['DEPT']
+        return x, y
 
     # def PrintGrathic(self):
     #     fig, ax1 = plt.subplots(figsize=(12, 4))
@@ -66,8 +50,9 @@ class HandlerDataFrame():
 
 def Main():
     las1 = HandlerDataFrame('1.las')
+    las2 = HandlerDataFrame('2.las')
     print(las1.GetCarrotValue('PZ_T'))
-
+    print(las1.keys)
 
 
 if __name__ == '__main__':
