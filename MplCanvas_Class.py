203dce731f64a05ead0c43f05a8528e267f772f8
Extragenchik <kirillprorus@gmail.com>
2022-10-12 20:47:07 +0500

    V1.0

--- MplCanvas_Class.py
+++ MplCanvas_Class.py
@@ -5,6 +5,7 @@ matplotlib.use('Qt5Agg')
 
 import matplotlib.pyplot as plt
 import pylab
+import numpy as np
 
 from Handler_Class import Handler
 
@@ -35,15 +36,85 @@ class MplCanvases(FigureCanvas):
         i = 1
 
         for carottage in carottages:
-            if carottage != 'DEPT':
-                x, y = las.GetCarottageValue(carottage)
 
+            if carottage == 'GK_T' or carottage == 'BK_T':
                 pylab.subplot(1, len(carottages), i)
+                if 'GK_T' in carottages:
+                    x, y = las.GetCarottageValue('GK_T')
+                    carottages.pop(carottages.index('GK_T'))
+                    pylab.plot(x, y) # color = цвет
+
+                x, y = las.GetCarottageValue('BK_T')
+
+                pylab.plot(x, y) # color = цвет
+
                 pylab.grid(True)
-                pylab.plot(x, y)
                 pylab.gca().invert_yaxis()
-                pylab.title(carottage)
+                pylab.title('GK_T, BK_T')
 
                 i += 1
+
+            if carottage == 'PS_T' or carottage == 'IK_T':
+                pylab.subplot(1, len(carottages), i)
+                if 'PS_T' in carottages:
+                    x, y = las.GetCarottageValue('PS_T')
+                    carottages.pop(carottages.index('PS_T'))
+                    pylab.plot(x, y) # color = цвет
+
+                x, y = las.GetCarottageValue('IK_T')
+
+                pylab.plot(x, y) # color = цвет
+
+                pylab.grid(True)
+                pylab.gca().invert_yaxis()
+                pylab.title('PS_T, IK_T')
+
+                i += 1
+
+            if carottage == 'PZ_T' or carottage == 'MPZ_T' or carottage == 'MGZ_T' or carottage == 'MBK_T':
+                pylab.subplot(1, len(carottages), i)
+                if 'PZ_T' in carottages:
+                    x, y = las.GetCarottageValue('PZ_T')
+                    carottages.pop(carottages.index('PZ_T'))
+                    pylab.plot(x, y) # color = цвет
+
+                if 'MPZ_T' in carottages:
+                    x, y = las.GetCarottageValue('MPZ_T')
+                    carottages.pop(carottages.index('MPZ_T'))
+                    pylab.plot(x, y) # color = цвет
+
+                if 'MGZ_T' in carottages:
+                    x, y = las.GetCarottageValue('MGZ_T')
+                    carottages.pop(carottages.index('MGZ_T'))
+                    pylab.plot(x, y) # color = цвет
+
+                if 'MBK_T' in carottages:
+                    x, y = las.GetCarottageValue('MBK_T')
+                    carottages.pop(carottages.index('MBK_T'))
+                    pylab.plot(x, y) # color = цвет
+
+                pylab.grid(True)
+                pylab.gca().invert_yaxis()
+                pylab.title('PZ_T, MPZ_T,\nMGZ_T, MBK_T')
+
+                i += 1
+
+            for NamberGZ in range(1, 5):
+                if carottage == f'GZ{NamberGZ}_T':
+                    pylab.subplot(1, len(carottages), i)
+
+                    for NamberGZ in range(1,5):
+                        if f'GZ{NamberGZ}_T' in carottages:
+                            x, y = las.GetCarottageValue(f'GZ{NamberGZ}_T')
+                            carottages.pop(carottages.index(f'GZ{NamberGZ}_T'))
+                            pylab.plot(x, y) # color = цвет
+
+                    pylab.grid(True)
+                    pylab.gca().invert_yaxis()
+                    pylab.title('GZ1-5_T')
+
+                    i += 1
+
+
 
         super(MplCanvases, self).__init__(fig)
